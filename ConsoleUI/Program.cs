21a58b1be77f19da94794b8715b47e176234e4fe
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-04-30 14:57:19 +0200

    init commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+// Capture information about each guest
+// Info to capture: FirstName, LastName, MessageToHost
+// Once done, loop thorugh each guest and print their info. 
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         
+
+        }
+    }
+}
